e49009c3e53ae7384165d31791ffdbfae182d307
MattCiraolo <75963796+MattCiraolo@users.noreply.github.com>
2024-11-30 01:48:26 -0500

    Update Button_Test.ino

--- Tests/Button_Test.ino
+++ Tests/Button_Test.ino
@@ -1,32 +1,9 @@
-/*
-  Button
+// Note this is heavily referenced form an Aurdunio IDE preset test
 
-  Turns on and off a light emitting diode(LED) connected to digital pin 13,
-  when pressing a pushbutton attached to pin 2.
-
-  The circuit:
-  - LED attached from pin 13 to ground through 220 ohm resistor
-  - pushbutton attached to pin 2 from +5V
-  - 10K resistor attached to pin 2 from ground
-
-  - Note: on most Arduinos there is already an LED on the board
-    attached to pin 13.
-
-  created 2005
-  by DojoDave <http://www.0j0.org>
-  modified 30 Aug 2011
-  by Tom Igoe
-
-  This example code is in the public domain.
-
-  https://www.arduino.cc/en/Tutorial/BuiltInExamples/Button
-*/
-
-// constants won't change. They're used here to set pin numbers:
+// Set pin numbers 
 const int buttonPin = 12;  // the number of the pushbutton pin
 const int ledPin = 13;    // the number of the LED pin
 
-// variables will change:
 int buttonState = 0;  // variable for reading the pushbutton status
 
 void setup() {
